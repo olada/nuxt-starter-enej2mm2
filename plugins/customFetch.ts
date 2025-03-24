a4a61122cf795b9f0c6f4fa659093cf0903e28b3
olada <david.olah@gmail.com>
2025-03-24 15:14:26 +0100

    remove console log output

--- plugins/customFetch.ts
+++ plugins/customFetch.ts
@@ -1,9 +1,7 @@
 export default defineNuxtPlugin((nuxtApp) => { 
-    console.log('This is being provisioned...');
     const api = $fetch.create({
       async onResponseError({ response }) {
         if (response.status === 401) {
-            console.log('NAVIGATE!');
           await nuxtApp.runWithContext(() => navigateTo('/login'))
         }
       }
